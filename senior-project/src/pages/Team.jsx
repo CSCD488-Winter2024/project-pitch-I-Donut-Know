15c369a4348b76391ed6e1e0ba381b3d90cbea73
Omar Facundo <114097196+omar5o9@users.noreply.github.com>
2024-01-31 21:37:38 +0000

    e

--- senior-project/src/pages/Team.jsx
+++ senior-project/src/pages/Team.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import '../styles/Team.css'
 import donut from '../images/donut.png'
 import monkey from '../media/monkey.mp4'
+import omar from '../images/mudryk.jpg'
 
 
 function Team() {
@@ -19,6 +20,11 @@ function Team() {
 
             <p>Meet the Team:</p>
             <video src={monkey} controls width="300" height="560" ></video>
+
+            <p>Manny:</p>
+            <p>Beatz:</p>
+            <p>Omar: Yo</p>
+            <img src={omar} alt="" />
 
             
         </section>
